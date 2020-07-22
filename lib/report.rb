bd3bb3bb206e66bdf6230aef0cb2effb6298b1cd
Sophie <sophiebrown438@gmail.com>
2020-07-22 13:23:20 +0100

    Pass test for single grades

--- lib/report.rb
+++ lib/report.rb
@@ -1,7 +1,12 @@
 class Report
+
   def view(string)
-    if string = "Green"
+    if string == "Green"
       "Green: 1"
+    elsif string == "Amber"
+      "Amber: 1"
+    else
+      "Red: 1"
     end
   end
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -7,4 +7,12 @@ describe Report do
   it "should it return Green: 1" do
     expect(report.view("Green")).to eq("Green: 1")
   end
+
+  it "should it return Amber: 1" do
+    expect(report.view("Amber")).to eq("Amber: 1")
+  end
+
+  it "should it return Red: 1" do
+    expect(report.view("Red")).to eq("Red: 1")
+  end
 end 
